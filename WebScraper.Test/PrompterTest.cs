31f3bae8f52f6ee79aa711d7618857ea0be27c67
mikel <mikel@MIKESPC>
2023-08-20 15:33:52 -0500

    Infrastructure #3 - cleaned up references

--- WebScraper.Test/PrompterTest.cs
+++ WebScraper.Test/PrompterTest.cs
@@ -8,11 +8,6 @@ namespace WebScraper.Test
         [Fact]
         public void PrompterReturnsSiteAndCategoryStrings()
         {
-            Prompter prompter = new();
-            (string site, string category) result = prompter.Prompt();
-
-            Assert.Equal(typeof(string), result.site.GetType());
-            Assert.Equal(typeof(string), result.category.GetType());
         }
     }
 }
